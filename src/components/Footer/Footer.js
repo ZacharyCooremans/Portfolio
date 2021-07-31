b357aa1bf7697104eb72becb18ac304c12348367
ZacharyCooremans <77285580+ZacharyCooremans@users.noreply.github.com>
2021-07-31 17:37:25 -0500

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+
+import { SocialIcons } from '../Header/HeaderStyles';
+import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel: 512-589-7488"> 512-589-7488</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:zcooremans@gmail.com"> zcooremans@gmail.com</LinkItem>
+        </LinkColumn>
+        <SocialIconsContainer>
+          <CompanyContainer>
+            <Slogan>Just trying to get it right</Slogan>
+          </CompanyContainer>
+          <SocialContainer>
+            <SocialIcons href="https://github.com/ZacharyCooremans">
+              <AiFillGithub size="3rem" />
+            </SocialIcons>
+            <SocialIcons href="https://www.linkedin.com/in/zachary-cooremans-a26065208/">
+              <AiFillLinkedin size="3rem" />
+            </SocialIcons>
+            <SocialIcons href="https://github.com">
+              <AiFillGithub size="3rem" />
+            </SocialIcons>
+          </SocialContainer>
+        </SocialIconsContainer>
+      </LinkList>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
